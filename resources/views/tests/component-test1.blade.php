d68ed581bd324450173132c8a91ee38b2e241e7a
yuki fujimoto <tenriyu0416@gmail.com>
2023-01-29 09:01:48 +0900

    4日

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,6 +2,6 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー1</x-slot>
 コンポーネントテスト1
-<x-tests.card title="タイトル" content="本文"/>
-
+<x-tests.card title="タイトル" content="本文1" :message="$message"/>
+<x-tests.card title="タイトル2"/>
 </x-tests.app>
