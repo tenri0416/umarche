cb513e0a6fe50321f4bc08cf78f73e22c9402933
yuki fujimoto <tenriyu0416@gmail.com>
2023-02-11 10:58:35 +0900

    10日目

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -38,6 +38,9 @@
                                             <th
                                                 class="px-4 py-3  title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
                                             </th>
+                                            <th
+                                                class="px-4 py-3  title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
+                                            </th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -47,11 +50,21 @@
                                                 <td class="px-4 py-3"> {{ $owner->email }}</td>
                                                 <td class="px-4 py-3"> {{ $owner->created_at }}</td>
 
+
                                                 <td class="px-4 py-3">
                                                     <button
                                                         onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id]) }}'"
                                                         class=" text-white bg-blue-300 border-0 py-2 px-4 focus:outline-none hover:bg-blue-500 rounded ">編集</button>
                                                 </td>
+                                                <form id="delete_{{ $owner->id }}" method="post"
+                                                    action="{{ route('admin.owners.destroy', ['owner' => $owner->id]) }}">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <td class="px-4 py-3">
+                                                        <a data-id="{{ $owner->id }}" onclick="deletePost(this)"
+                                                            class=" text-white bg-red-300 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded ">削除</a>
+                                                    </td>
+                                                </form>
                                             </tr>
                                         @endforeach
                                     </tbody>
@@ -78,4 +91,13 @@
             </div>
         </div>
     </div>
+
+    <script>
+        function deletePost(e) {
+            'use strict';
+            if (confirm('本当に削除してもいいですか?')) {
+                document.getElementById('delete_' + e.dataset.id).submit();
+            }
+        }
+    </script>
 </x-app-layout>
