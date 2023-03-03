952da3cf0f649117247e504da942e12f5281d9d7
yuki fujimoto <tenriyu0416@gmail.com>
2023-03-03 21:47:10 +0900

    17日目

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -21,13 +21,7 @@
                                         @endif
                                     </div>
                                     <div class="text-xl">{{ $shop->name }}</div>
-                                    <div>
-                                        @if (empty($shop->filename))
-                                            <img src="{{ asset('images/no_image.jpg') }}">
-                                        @else
-                                            <img src="{{ asset('storage/sbops/' . $shop->filename) }}">
-                                        @endif
-                                    </div>
+                                    <x-shop-thumbnail />
                                 </div>
                             </a>
                         </div>
